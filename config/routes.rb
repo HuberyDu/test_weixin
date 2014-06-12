30418a05ceb567c442c3e05bcf6950233ea073a5
HuberyDu <duxiaolong92@gmail.com>
2014-06-12 23:29:31 +0800

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 # -*- encoding : utf-8 -*-
 TestWeixin::Application.routes.draw do
-  resources :weixin, only:[:show] do
-     collection do
-       get "init_menu"
-     end
-  end
-
-  scope :path => "/weixin", :via => :post do 
-    root :to => 'weixin#text_message', :constraints => Weixin::Router.new(:type => "text")
-    root :to => 'weixin#image_message', :constraints => Weixin::Router.new(:type => "image")
-    root :to => 'weixin#subscribe_message', :constraints => Weixin::Router.new(:type => "event", :event => "CLICK")
-    root :to => 'weixin#click_menu_message', :constraints => Weixin::Router.new(:type => "event", :event => "subscribe")
-    root :to => 'weixin#create'
-  end
+  resources :weixin
 end

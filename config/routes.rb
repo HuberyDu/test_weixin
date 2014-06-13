a3a997a7e40cdea552068a55798c26fab66373ef
HuberyDu <duxiaolong92@gmail.com>
2014-06-13 11:06:45 +0800

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # -*- encoding : utf-8 -*-
 TestWeixin::Application.routes.draw do
-  resources :weixin, only:[:show] do
-     collection do
-       get "init_menu"
-     end
-  end
+  # resource :weixin, only:[:show] do
+  #    collection do
+  #      get "init_menu"
+  #    end
+  # 
+  get 'weixin' => "weixin#show"
+  get 'weixin/init_menu' => "weixin#init_menu"
  
   scope :path => "/weixin", :via => :post do 
     root :to => 'weixin#text_message', :constraints => Weixin::Router.new(:type => "text")

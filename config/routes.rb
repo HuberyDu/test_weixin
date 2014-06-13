0e87a14d88e33f426d06ac16ff77549235f80f20
HuberyDu <duxiaolong92@gmail.com>
2014-06-13 12:00:43 +0800

    m

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ TestWeixin::Application.routes.draw do
   scope :path => "/weixin", :via => :post do 
     root :to => 'weixin#text_message', :constraints => Weixin::Router.new(:type => "text")
     root :to => 'weixin#image_message', :constraints => Weixin::Router.new(:type => "image")
-    root :to => 'weixin#subscribe_message', :constraints => Weixin::Router.new(:type => "event", :event => "CLICK")
-    root :to => 'weixin#click_menu_message', :constraints => Weixin::Router.new(:type => "event", :event => "subscribe")
+    root :to => 'weixin#subscribe_message', :constraints => Weixin::Router.new(:type => "event", :event => "subscribe")
+    root :to => 'weixin#click_menu_message', :constraints => Weixin::Router.new(:type => "event", :event => "CLICK")
   end
 end

d4f27eb593bbdd9a02f44503797b3f0f5e766c25
HuberyDu <duxiaolong92@gmail.com>
2014-06-13 16:54:28 +0800

    m

--- config/initializers/weixin_router.rb
+++ config/initializers/weixin_router.rb
@@ -3,19 +3,23 @@ module Weixin
   # 微信内部路由规则类，用于简化配置
   class Router
     def initialize(options)
-      @type = options[:type] if options[:type]
+      @message_type = options[:type] if options[:message_type]
       @event = options[:event] if options[:event]
+      @event_key = options[:event_key] if options[:event_key]
+      @content = options[:content] if options[:content]
     end
 
     def matches?(request)
       xml = request.params[:xml]
-      result = false
-      puts "----------#{request.params}---------------"
       if xml && xml.is_a?(Hash)
-        result = @type == xml[:MsgType] && @event == xml[:Event]
+        (return false unless @message_type == xml[:MsgType]) if @message_type
+        (return false unless @event == xml[:Event]) if @event
+        (return false unless @event_key == xml[:EventKey]) if @event_key && @event_key.is_a?(String)
+        (return false unless @event_key =~ xml[:EventKey]) if @event_key && @event_key.is_a?(Regexp)
+        (return false unless @content == xml[:Content]) if @content && @content.is_a?(String)
+        (return false unless @content =~ xml[:Content]) if @content && @content.is_a?(Regexp)
       end
-      puts "------#{request.params[:xml][:type]}}------" if result
-      return result
+      return true
     end
   end    
 end 
